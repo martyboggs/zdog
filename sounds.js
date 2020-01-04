b0955b76577fc206c12a88681f291545be034ced
martyboggs <whack47@gmail.com>
2020-01-03 20:30:02 -0800

    vol

--- sounds.js
+++ sounds.js
@@ -6,6 +6,7 @@ var magic = $buzz({
 var music = $buzz({
 	src: 'sounds/CantinaBand60.wav',
 	loop: true,
+	volume: 0.5,
 });
 music.play()
 
@@ -15,5 +16,6 @@ var sound = $buzz({
 
 var keysound = $buzz({
 	src: 'sounds/keysound.wav',
+	volume: 1.2,
 });
 
