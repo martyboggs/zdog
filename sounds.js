24d5b11b278157e30dc40c1c7c1c3a6badac32df
martyboggs <whack47@gmail.com>
2020-01-03 20:33:04 -0800

    preload

--- sounds.js
+++ sounds.js
@@ -1,8 +1,3 @@
-var magic = $buzz({
-	src: 'sounds/magic.wav',
-	loop: true,
-});
-
 var music = $buzz({
 	src: 'sounds/CantinaBand60.wav',
 	loop: true,
@@ -10,12 +5,20 @@ var music = $buzz({
 });
 music.play()
 
+var magic = $buzz({
+	src: 'sounds/magic.wav',
+	loop: true,
+	preload: true,
+});
+
 var sound = $buzz({
 	src: 'sounds/sound.wav',
+	preload: true,
 });
 
 var keysound = $buzz({
 	src: 'sounds/keysound.wav',
 	volume: 1.2,
+	preload: true,
 });
 
